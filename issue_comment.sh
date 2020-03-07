83f5cf993afc2be54006894bf39b94b73ec116b0
Gábor Vecsei <vecseigabor.x@gmail.com>
2020-03-07 14:58:03 +0100

    issue comment script changed

--- issue_comment.sh
+++ issue_comment.sh
@@ -11,12 +11,19 @@ ISSUE_NUMBER=$2
 
 URI=https://api.github.com
 API_VERSION=v3
-API_HEADER="Accept: application/vnd.github.${API_VERSION}+json"
+# API_HEADER="Accept: application/vnd.github.${API_VERSION}+json"
+CONTENT_TYPE_HEADER="Content-Type: application/json"
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 
-# Create a comment with APIv3 # POST /repos/:owner/:repo/issues/:issue_number/comments
-curl -XPOST -sSL \
+echo 'curl -X POST -sSL \
     -d "{\"body\": \"$MESSAGE\"}" \
     -H "${AUTH_HEADER}" \
-    -H "${API_HEADER}" \
+    -H "${CONTENT_TYPE_HEADER}" \
+    "${URI}/repos/${GITHUB_REPOSITORY}/issues/${ISSUE_NUMBER}/comments"'
+
+# https://developer.github.com/v3/issues/comments/#create-a-comment
+curl -X POST -sSL \
+    -d "{\"body\": \"$MESSAGE\"}" \
+    -H "${AUTH_HEADER}" \
+    -H "${CONTENT_TYPE_HEADER}" \
     "${URI}/repos/${GITHUB_REPOSITORY}/issues/${ISSUE_NUMBER}/comments"
