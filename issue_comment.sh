778cc27ff5eb97526b55cd32a94565c55ef1b3ef
Gábor Vecsei <vecseigabor.x@gmail.com>
2020-03-07 15:01:21 +0100

    fix

--- issue_comment.sh
+++ issue_comment.sh
@@ -6,20 +6,16 @@
 # - GITHUB_REPOSITORY (This is automatically created in Github Actions)
 # $ issue_comment.sh <<Text Comment>> <<Issue number>>
 
-MESSAGE=$1
-ISSUE_NUMBER=$2
+MESSAGE="$1"
+echo "Message: ${MESSAGE}"
+ISSUE_NUMBER="$2"
+echo "Issue Number: ${ISSUE_NUMBER}"
 
 URI=https://api.github.com
 API_VERSION=v3
 # API_HEADER="Accept: application/vnd.github.${API_VERSION}+json"
 CONTENT_TYPE_HEADER="Content-Type: application/json"
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
-
-echo 'curl -X POST -sSL \
-    -d "{\"body\": \"$MESSAGE\"}" \
-    -H "${AUTH_HEADER}" \
-    -H "${CONTENT_TYPE_HEADER}" \
-    "${URI}/repos/${GITHUB_REPOSITORY}/issues/${ISSUE_NUMBER}/comments"'
 
 # https://developer.github.com/v3/issues/comments/#create-a-comment
 curl -X POST -sSL \
