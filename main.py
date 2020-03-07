f60017e817c0f51c5224a14cd517436f837006cd
Gábor Vecsei <vecseigabor.x@gmail.com>
2020-03-07 15:09:42 +0100

    mini fix

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ parser.add_argument("--issue_comment_body", required=True)
 parser.add_argument("--issue_user", required=True)
 args = parser.parse_args()
 
-reply_message = f"Hey @{args.issue_user}\n\nThis was you comment: {args.issue_comment_body}"
+reply_message = f"Hey @{args.issue_user}\nThis was you comment: {args.issue_comment_body}"
 
-print(f"::set-output name=issue_comment_reply::'{reply_message}'")
+print(f"::set-output name=issue_comment_reply::{reply_message}")
