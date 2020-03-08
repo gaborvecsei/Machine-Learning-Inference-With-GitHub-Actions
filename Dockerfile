cf2e841e56591fd7ed1496ffe67bc8f71315ca74
Gábor Vecsei <vecseigabor.x@gmail.com>
2020-03-08 15:59:46 +0100

    model loading and forest classifier prediction implemented

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,6 @@ RUN pip install numpy scikit-learn
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
+COPY random_forest_model.pkl /random_forest_model.pkl
+
 ENTRYPOINT ["/entrypoint.sh"]
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import argparse
+import numpy as np
+import pickle
 
 
 def parse_comment_input(comment: str) -> list:
@@ -12,14 +14,21 @@ def parse_comment_input(comment: str) -> list:
     return parsed_numbers
 
 
-def make_prediction(sepal_length: float, sepal_width: float, petal_length: float, petal_width: float) -> int:
-    # TODO:  implement me
-    return 100
+def load_model(model_path: str):
+    with open(model_path, 'rb') as file:
+        model = pickle.load(file)
+    return model
+
+
+def make_prediction(model, sepal_length: float, sepal_width: float, petal_length: float, petal_width: float) -> int:
+    x = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    pred = model.predict(x)[0]
+    return pred
 
 
 def map_class_id_to_name(class_id: int) -> str:
-    # TODO: implement me
-    return "Something"
+    class_id_to_name_dict = {0: "Setosa", 1: "Versicolor", 2: "Virginica"}
+    return class_id_to_name_dict[class_id]
 
 
 parser = argparse.ArgumentParser()
@@ -28,9 +37,11 @@ parser.add_argument("--issue_comment_body", required=True)
 parser.add_argument("--issue_user", required=True)
 args = parser.parse_args()
 
+model = load_model("/random_forest_model.pkl")
+
 try:
     sepal_length, sepal_width, petal_length, petal_width = parse_comment_input(args.issue_comment_body)
-    predicted_class_id = make_prediction(sepal_length, sepal_width, petal_length, petal_width)
+    predicted_class_id = make_prediction(model, sepal_length, sepal_width, petal_length, petal_width)
     predicted_class_name = map_class_id_to_name(predicted_class_id)
     reply_message = f"Hey @{args.issue_user}!<br><br>This was your input: {args.issue_comment_body}.<br>The prediction: *{predicted_class_name}*"
 except Exception as e:  # TODO: Too broad exception type
